a45715e48b0fda148256a1cedffb6f34526d1ba9
Sanish Sangeeth <sanishsangeeth973@gmail.com>
2024-08-18 22:31:51 +0530

    new files added!

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import tkinter
 import customtkinter as tk
 import pyttsx3
 
-
+# funtion fot the speak button
 def speak():
     text = text_entry.get()
     engine = pyttsx3.init()
@@ -10,17 +10,27 @@ def speak():
     engine.runAndWait()
 
 
-
+# app window config
 app = tk.CTk()
 app.title('Text-to-Speech')
 app.geometry('400x300')
 app.iconbitmap('text-to-speech/resources/text-to-voice.ico')
 
-
-text_entry = tk.CTkEntry(app,width=300,height=100,justify=tk.CENTER,placeholder_text='type here to convert into speech')
+# Entry label on window
+text_entry = tk.CTkEntry(app,
+             width=300,
+             height=100,
+             justify=tk.CENTER,
+             placeholder_text='type here to convert into speech')
 text_entry.pack(pady=10,padx=10)
 
-speak_button = tk.CTkButton(app,text='speak',command=speak,fg_color='#a2d2ff',width=10,height=10)
+# Speak label on window
+speak_button = tk.CTkButton(app,
+                           text='speak',
+                           command=speak,
+                           fg_color=('#00afb9','#a2d2ff'),
+                           width=10,
+                           height=10)
 speak_button.pack(pady=10)
 
 app.mainloop()
