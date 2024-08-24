3e1f40cd2e67f699041a5c9d78b10d943d663f7f
Sanish Sangeeth <sanishsangeeth973@gmail.com>
2024-08-24 22:20:31 +0530

    revidsed main.py

--- src/main.py
+++ src/main.py
@@ -2,12 +2,17 @@ import tkinter
 import customtkinter as tk
 import pyttsx3
 
+
 # funtion fot the speak button
 def speak():
     text = text_entry.get()
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
+
+def rate():
+    pass
+
 
 
 # app window config
@@ -16,8 +21,12 @@ app.title('Text-to-Speech')
 app.geometry('400x300')
 app.iconbitmap('text-to-speech/resources/text-to-speech.ico')
 
+# Window Frame
+frame = tk.CTkFrame(app)
+frame.pack(padx=10,pady=10)
+
 # Entry label on window
-text_entry = tk.CTkEntry(app,
+text_entry = tk.CTkEntry(frame,
              width=300,
              height=100,
              justify=tk.CENTER,
@@ -25,7 +34,7 @@ text_entry = tk.CTkEntry(app,
 text_entry.pack(pady=10,padx=10)
 
 # Speak label on window
-speak_button = tk.CTkButton(app,
+speak_button = tk.CTkButton(frame,
                            text='speak',
                            command=speak,
                            fg_color=('#00afb9'),
@@ -33,6 +42,13 @@ speak_button = tk.CTkButton(app,
                            width=100,
                            height=40)
 speak_button.pack(pady=20)
+
+# Slider to adjust speed of voice
+#slider_button = tk.CTkSlider(frame,
+#                             from_=0,to=100,
+ #                            orient=tk.HORIZONTAL,
+#                             command=rate)
+# slider_button.pack(padx=10,pady=10)
 
 
 if __name__ == '__main__':
---
+++ tests/tests_main.py
@@ -0,0 +1,12 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+
+# Try different voices
+for voice in voices:
+    engine.setProperty('voice', voice.id)
+    engine.say("Hello, world!")
+    engine.runAndWait()
+    print("Voice:", voice.name)
